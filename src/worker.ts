73dc16f21b53fd59eb32bc2eb304a6f335bf6bcf
Alek Evans <gpg@alekeagle.com>
2022-03-11 13:53:39 -0700

    no more crash thanks

--- src/worker.ts
+++ src/worker.ts
@@ -12,21 +12,6 @@ if (worker.isMainThread) throw new Error("can't be ran as main thread");
       worker.parentPort.postMessage(415);
       process.exit(0);
     }
-    if (a.mime.startsWith('video') || a.mime.startsWith('image')) {
-      exec(
-        `ffmpeg -i /var/www-uploads/${worker.workerData.file} -vf 'scale=256:256:force_original_aspect_ratio=1,format=rgba,pad=256:256:(ow-iw)/2:(oh-ih)/2:color=#00000000' -vframes 1 /tmp/cumulonimbus-preview-cache/${worker.workerData.file}.webp`,
-        (error, stdout, stderr) => {
-          if (error) {
-            worker.parentPort.postMessage(500);
-            console.error(error, stderr, stdout);
-            process.exit(0);
-          } else {
-            worker.parentPort.postMessage(200);
-            process.exit(0);
-          }
-        }
-      );
-    }
     if (worker.workerData.file.match(/\.html?$/) || a.mime === 'application/pdf') {
       exec(
         `chromium --headless --window-size=256,256 --screenshot=/tmp/cumulonimbus-preview-cache/${worker.workerData.file}.webp file:///var/www-uploads/${worker.workerData.file}`,
@@ -44,6 +29,21 @@ if (worker.isMainThread) throw new Error("can't be ran as main thread");
     } else {
       worker.parentPort.postMessage(415);
       process.exit(0);
+    }
+    if (a.mime.startsWith('video') || a.mime.startsWith('image')) {
+      exec(
+        `ffmpeg -i /var/www-uploads/${worker.workerData.file} -vf 'scale=256:256:force_original_aspect_ratio=1,format=rgba,pad=256:256:(ow-iw)/2:(oh-ih)/2:color=#00000000' -vframes 1 /tmp/cumulonimbus-preview-cache/${worker.workerData.file}.webp`,
+        (error, stdout, stderr) => {
+          if (error) {
+            worker.parentPort.postMessage(500);
+            console.error(error, stderr, stdout);
+            process.exit(0);
+          } else {
+            worker.parentPort.postMessage(200);
+            process.exit(0);
+          }
+        }
+      );
     }
   } catch (e) {
     worker.parentPort.postMessage(500);
