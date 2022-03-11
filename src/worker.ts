978b3ece37f7bda2c05e8086711c31d0fe142d9d
Alek Evans <github@alekeagle.com>
2022-03-11 14:36:17 -0700

    unfuck?

--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,7 @@
 import { exec } from 'child_process';
 import worker from 'worker_threads';
 import fileType from 'file-type';
+import puppeteer from 'puppeteer';
 
 if (worker.isMainThread) throw new Error("can't be ran as main thread");
 (async function () {
@@ -12,31 +13,20 @@ if (worker.isMainThread) throw new Error("can't be ran as main thread");
       worker.parentPort.postMessage(415);
       process.exit(0);
     }
-    if (worker.workerData.file.match(/\.html?$/) || a.mime === 'application/pdf') {
+    if (
+      worker.workerData.file.match(/\.html?$/) ||
+      a.mime === 'application/pdf'
+    ) {
+      const browser = await puppeteer.launch(),
+        page = await browser.newPage();
+      await page.goto(`file:///var/www-uploads/${worker.workerData.file}`, {
+        waitUntil: 'networkidle2'
+      });
+      await page.screenshot({
+        path: `/tmp/cumulonimbus-preview-cache/${worker.workerData.file}.webp`
+      });
       exec(
-        `chromium --headless --window-size=256,256 --screenshot=/tmp/cumulonimbus-preview-cache/${worker.workerData.file}.webp file:///var/www-uploads/${worker.workerData.file}`,
-        (error, stdout, stderr) => {
-          if (error) {
-            worker.parentPort.postMessage(500);
-            console.error(error, stderr, stdout);
-            process.exit(0);
-          } else {
-            console.log(stdout);
-            exec(`chmod +r+w /tmp/cumulonimbus-preview-cache/${worker.workerData.file}.webp`,
-            (error, stdout, stderr) => {
-              if (error) {
-                worker.parentPort.postMessage(500);
-                console.error(error, stderr, stdout);
-                process.exit(0);
-              } else {
-                worker.parentPort.postMessage(200);
-                process.exit(0);
-              }
-            });
-          }
-        }
-      );
-      exec(`chmod +r+w /tmp/cumulonimbus-preview-cache/${worker.workerData.file}.webp`,
+        `chmod +r+w /tmp/cumulonimbus-preview-cache/${worker.workerData.file}.webp`,
         (error, stdout, stderr) => {
           if (error) {
             worker.parentPort.postMessage(500);
@@ -46,7 +36,8 @@ if (worker.isMainThread) throw new Error("can't be ran as main thread");
             worker.parentPort.postMessage(200);
             process.exit(0);
           }
-        });
+        }
+      );
     } else {
       worker.parentPort.postMessage(415);
       process.exit(0);
