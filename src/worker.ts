59c707c6a4a51e0e1b21f2c7c3c92b6cfac2cd72
AlekEagle <gpg@alekeagle.com>
2022-07-11 09:57:02 -0800

    actually make it work

--- src/worker.ts
+++ src/worker.ts
@@ -2,6 +2,7 @@ import { exec } from 'child_process';
 import worker from 'worker_threads';
 import fileType from 'file-type';
 import puppeteer from 'puppeteer';
+import { unlink } from 'fs/promises';
 
 if (worker.isMainThread) throw new Error("can't be ran as main thread");
 (async function () {
@@ -52,15 +53,27 @@ if (worker.isMainThread) throw new Error("can't be ran as main thread");
       );
     } else if (a.mime === 'application/pdf') {
       exec(
-        `pdftoppm -singlefile -png -x 0 -y 0 -W 256 -H 256 -scale-to 256 | ffmpeg -i - -vf 'scale=256:256:force_original_aspect_ratio=1,format=rgba,pad=256:256:(ow-iw)/2:(oh-ih)/2:color=#00000000' -vframes 1 /tmp/cumulonimbus-preview-cache/${worker.workerData.file}.webp`,
+        `pdftoppm -singlefile -png -x 0 -y 0 -W 256 -H 256 -scale-to 256 /var/www-uploads/${worker.workerData.file} /tmp/${worker.workerData.file}`,
         (error, stdout, stderr) => {
           if (error) {
             worker.parentPort.postMessage(500);
             console.error(error, stderr, stdout);
             process.exit(0);
           } else {
-            worker.parentPort.postMessage(200);
-            process.exit(0);
+            exec(
+              `ffmpeg -i /tmp/${worker.workerData.file}.png -vf 'scale=256:256:force_original_aspect_ratio=1,format=rgba,pad=256:256:(ow-iw)/2:(oh-ih)/2:color=#00000000' -vframes 1 /tmp/cumulonimbus-preview-cache/${worker.workerData.file}.webp`,
+              async (error, stdout, stderr) => {
+                if (error) {
+                  worker.parentPort.postMessage(500);
+                  console.error(error, stderr, stdout);
+                  process.exit(0);
+                } else {
+                  await unlink(`/tmp/${worker.workerData.file}.png`);
+                  worker.parentPort.postMessage(200);
+                  process.exit(0);
+                }
+              }
+            );
           }
         }
       );
